7ae31c698360d2c56b3d3f59539cdf1a2f61f8f3
David Fowler <davidfowl@gmail.com>
2025-11-14 22:11:50 -0800

    Initial commit with static app

---
+++ yarpstatic/apphost.cs
@@ -0,0 +1,16 @@
+﻿#:package Aspire.Hosting.Docker@13-*
+#:package Aspire.Hosting.JavaScript@13.0.0
+#:package Aspire.Hosting.Yarp@13.0.0
+#:sdk Aspire.AppHost.Sdk@13.0.0
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddDockerComposeEnvironment("dc");
+
+var frontend = builder.AddViteApp("static", "./frontend");
+
+builder.AddYarp("app")
+           .WithExternalHttpEndpoints()
+           .PublishWithStaticFiles(frontend);
+
+builder.Build().Run();
